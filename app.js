7ed4c39fbf092191eb1d9ca88b6e62ea9a7ba81d
myjournal <getmyjournal@gmail.com>
2015-02-03 09:03:14 +0000

    initial push

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', []);
+app.factory("category", function() {
+    return {
+        types: ["Accordion", "Table", "Tree", "Calendar", "Slider", "Splitter"]
+    };
+});
+app.directive("list", ['category', function(category) {
+
+    return {
+        restrict: 'E',
+        /* restrict this directive to elements */
+        scope: {
+            multiple: "@",
+            filter: "@"
+        },
+        template: '<input type="text" ng-model="stringofitems" class="input-text" />' + '<ul class="dropdown">' +
+            '<li ng-show={{(filter=="true")}}><input type="text" class="search-text" ng-model="query" /></li>' +
+            '<li ng-repeat="item in types|filter:query"><a class="item-type" href="javascript:void(0)">' +
+            '<input ng-attr-type={{(multiple=="true")?"checkbox":"radio"}} ng-model="confirmed"' +
+            ' name="controls" ng-checked="ischecked(item)"  ng-change="selectionToggle(confirmed,item)" />' +
+            '{{item}}</a></li></ul>',
+        controller: function($scope) {
+            $scope.confirmed = false;
+            $scope.types = category.types;
+            $scope.items = [];
+            $scope.ischecked = function(item) {
+
+                return $scope.items.indexOf(item) > -1;
+            };
+            $scope.selectionToggle = function(confirmed, item) {
+                if (confirmed) {
+                    $scope.items.push(item);
+                } else {
+                    var index = $scope.items.indexOf(item);
+                    if (index > -1) {
+                        $scope.items.splice(index, 1);
+                    }
+                    //radio button logic
+                    else {
+                        $scope.items = [];
+                        $scope.items.push(item);
+                    }
+                }
+                $scope.stringofitems = $scope.items.toString();
+            };
+        }
+    };
+}]);
